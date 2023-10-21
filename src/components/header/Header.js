91663e4668836ec9318979c59bcf0d80841a95c6
ghhjhkjkklkl <99083436+ghhjhkjkklkl@users.noreply.github.com>
2023-10-21 20:47:16 +0200

    1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,20 @@
-import "./header.scss";
+import './header.scss';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">
-        <span className="header__title header__title--color">Marvel</span>{" "}
+        <span className="header__title header__title--color">Marvel</span>{' '}
         information portal
       </h1>
       <ul className="header__list">
         <li className="header__item">
-          <a className="header__link active" href="">
+          <a className="header__link active" href="#">
             Characters
           </a>
         </li>
         <li className="header__item">
-          <a className="header__link" href="">
+          <a className="header__link" href="#">
             Comics
           </a>
         </li>
